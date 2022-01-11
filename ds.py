74cea3d9ec84bbab79e71bc17b99729a646bc0c3
Souhail <souhail.ait_lahcen@edu.devinci.fr>
2022-01-11 12:15:45 +0100

    Ajout du set

--- ds.py
+++ ds.py
@@ -1,14 +1,12 @@
 import redis
 import sys
+import random
 r = redis.Redis(host='localhost', port=6379, db=0)
 liste = []
 choice = {}
+
 #STEP 1
-
-
 def exec_menu(choix):
-    
-    
     ch = choix.lower()
     if ch == '':
         choice['main_menu']()
@@ -19,23 +17,34 @@ def exec_menu(choix):
             print ("Invalid selection, please try again.\n")
             choice['menu']()
     return
+
 def inputs():
+    
     user=input("Username : ")
     pwd=input("Password : ")
     age=input("Age : ")
-    loc = input("location:")
-    sex = input('gender :')
-    email=input("E-mail adress: ")
-    postal = input("postal :")
-    return [user,pwd,age,loc,sex,email,postal]   
+    loc = input("location : ")
+    sex = input('gender : ')
+    email=input("E-mail adress : ")
+    postal = input("postal : ")
+    return ["Username",user,"Password",pwd,"Age",age,"Location",loc,"Gender",sex,"E-mail",email,"Postal",postal]   
+
 def signIn():
-    a = inputs()
+    i = inputs()
     liste = []
-    liste.append(a)
+    liste.append(i)
     #=>ajouter l'individu
-    #r.set(i[0][0], i[0].split())
+    ind="Individu"
+    iden=random.randint(1,15343048)
+    res=ind+":"+str(iden)
+    delimiteur = ' '
+    s = delimiteur.join(i)
+    #print(res,s)
+    r.set(res, i[0].split())
+
 def exit():
     sys.exit()
+
 def menurinc():
     print("1 : sign in ")
     print("2 : leave")
